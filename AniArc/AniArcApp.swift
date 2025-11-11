033eaf4d6eafa000e5c37af69bd0354a93258205
Harlen Postill <harlen.postill@student.uts.edu.au>
2025-11-11 11:09:30 +1100

    Initial Commit

---
+++ AniArc/AniArcApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniArcApp.swift
+//  AniArc
+//
+//  Created by Harlen Postill on 11/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AniArcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
